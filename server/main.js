fb73e2f1036e3011e0c687833fb31f680e4c7583
Yves Schubert <yves.schubert@posteo.de>
2019-05-30 20:01:48 +0200

    #5 use other excel export library as the old one broke reactiveness

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import {Meteor} from "meteor/meteor";
 import {COLLECTIONS} from "../imports/collections";
-
+import * as Excel from "node-excel-export";
 import "../imports/methods";
 
 Meteor.startup(() => {
@@ -32,25 +32,60 @@ Meteor.publish(COLLECTIONS.STORIES, sessionId => {
 });
 
 Router.route("/session/download/:sessionId", function () {
-    const data = Stories.find({sessionId: this.params.sessionId}).fetch();
-    const fields = [
-        {
-            key: "name",
-            title: "Name",
-        },
-        {
-            key: "estimate",
-            title: "Schätzung",
-            type: "number"
-        }
-    ];
 
-    const title = "Stories";
-    const file = Excel.export(title, fields, data);
+    const styles = {
+        header: {
+            fill: {
+                fgColor: {
+                    rgb: 'FFFFFFFF'
+                }
+            },
+            font: {
+                color: {
+                    rgb: 'FF000000'
+                },
+                bold: true,
+                underline: false
+            }
+        }
+    };
+
+    const specification = {
+        name: {
+            displayName: 'Name',
+            width: '50',
+            headerStyle: styles.header
+        },
+        estimate: {
+            displayName: 'Schätzung',
+            width: '10',
+            headerStyle: styles.header
+        }
+    };
+
+    const session = Sessions.findOne(this.params.sessionId);
+    const dataset = Stories.find({sessionId: this.params.sessionId}, {
+        fields: {
+            "name": 1,
+            "estimate": 1
+        }
+    }).fetch();
+
+    const report = Excel.buildExport(
+        [
+            {
+                name: session.name,
+                specification: specification,
+                data: dataset
+            }
+        ]
+    );
+
     const headers = {
         "Content-type": "application/vnd.openxmlformats",
-        "Content-Disposition": "attachment; filename=" + title + ".xlsx"
+        "Content-Disposition": "attachment; filename=Stories.xlsx"
     };
     this.response.writeHead(200, headers);
-    this.response.end(file, "binary");
+    this.response.end(report, "binary");
+
 }, {where: "server"});
