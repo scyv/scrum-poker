d2fcb1e6cbe70045642ab7a29c40055e8187cc0f
Yves Schubert <yves.schubert@iteratec.de>
2019-05-30 18:01:20 +0200

    Fix #6: Show QR Code for participation

--- client/shareLink.js
+++ client/shareLink.js
@@ -1,11 +1,21 @@
+function currentLink() {
+    const url = Router.current().originalUrl;
+    return url.startsWith('http') ? url : (location.origin + url);
+}
+
+Template.shareLink.onRendered(function () {
+    $('#qrcode').qrcode({
+        size: 300,
+        text: currentLink()
+    });
+});
 Template.shareLink.helpers({
     currentLink() {
-        const url = Router.current().originalUrl;
-        return url.startsWith('http') ? url : (location.origin + url);
+        return currentLink();
     }
 });
 Template.shareLink.events({
-   'click .currentLink'() {
-       $(".currentLink").select();
-   }
+    'click .currentLink'() {
+        $(".currentLink").select();
+    }
 });
